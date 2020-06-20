3f53a92a242f677c334a07c2ad9d28ca98034ab0
accidentlywoo <accidentlywoo@gmail.com>
2020-06-20 13:22:31 +0900

    일반적인 Comparator사용 방법 feat. Static

--- algorithm/src/youngri/ComparatorPractice.java
+++ algorithm/src/youngri/ComparatorPractice.java
@@ -11,6 +11,17 @@ public class ComparatorPractice {
             this.name = name;
             this.quantity = quantity;
         }
+        public static Comparator<Fruit> nameComparator = new Comparator<Fruit>() {
+            @Override // Anonymous Class
+            public int compare(Fruit fruit1, Fruit fruit2) {
+                return fruit1.name.compareTo(fruit2.name);
+            }
+        };
+        public static Comparator<Fruit> quantityComparator = new Comparator<Fruit>() {
+            public int compare(Fruit fruit1, Fruit fruit2) {
+                return fruit1.quantity - fruit2.quantity;
+            }
+        };
         @Override
         public String toString() {
             return "Fruit{" +
@@ -18,31 +29,5 @@ public class ComparatorPractice {
                     ", quantity=" + quantity +
                     '}';
         }
-    }
-
-    public static void main(String[] args) {
-        Fruit[] fruits = new Fruit[4];
-        fruits[0] = new Fruit("Pineapple",70);
-        fruits[1] = new Fruit("Apple",100);
-        fruits[2] = new Fruit("Orange",80);
-        fruits[3] = new Fruit("Banana",90);
-
-        Comparator<Fruit> nameComparator = new Comparator<Fruit>() {
-            @Override // Anonymous Class
-            public int compare(Fruit fruit1, Fruit fruit2) {
-                return fruit1.name.compareTo(fruit2.name);
-            }
-        };
-        /*
-        제네릭 프로그래밍
-         */
-        Arrays.sort(fruits, nameComparator);
-        System.out.println(Arrays.toString(fruits));
-        Arrays.sort(fruits, new Comparator<Fruit>() {
-            public int compare(Fruit fruit1, Fruit fruit2) {
-                return fruit1.quantity - fruit2.quantity;
-            }
-        });
-        System.out.println(Arrays.toString(fruits));
     }
 }
